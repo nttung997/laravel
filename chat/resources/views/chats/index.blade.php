0d27ab91bfb6051d58711d333a687a0226ada374
tung <nttung997@gmail.com>
2020-10-06 11:57:49 +0700

    finish

--- chat/resources/views/chats/index.blade.php
+++ chat/resources/views/chats/index.blade.php
@@ -1,28 +1,11 @@
 <html>
 
 <body>
-    <form action="" method="post">
+    <form action="" method="post" id="theForm">
         @csrf
         <input type="text" name="content">
         <button type="submit">Submit</button>
     </form>
-
-    {{-- <table>
-        <thead>
-            <tr>
-                <th>Author</th>
-                <th>Content</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($chats as $chat)
-            <tr>
-                <td>{{$chat->author}}</td>
-    <td>{{$chat->content}}</td>
-    </tr>
-    @endforeach
-    </tbody>
-    </table> --}}
 
     <div id="data">
         @foreach ($chats as $chat)
@@ -33,18 +16,17 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
         integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
         crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"
+        integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.1/socket.io.js"
         integrity="sha512-AcZyhRP/tbAEsXCCGlziPun5iFvcSUpEz2jKkx0blkYKbxU81F+iq8FURwPn1sYFeksJ+sDDrI5XujsqSobWdQ=="
         crossorigin="anonymous"></script>
     <script>
+        $("#theForm").ajaxForm()
         var socket = io('http://localhost:6001')
-    socket.on('laravel_database_chat', function(data){
-        console.log(data)
-        if($('#'+data.id).length == 0){
-            $('#data').prepend('<p><strong>'+data.author+'</strong>: '+data.content+'</p>')
-        }else{
-            console.log('Da co tin nhan')
-        }
+        socket.on('laravel_database_chat', function(data){
+        $('#data').prepend('<p><strong>'+data.author+'</strong>: '+data.content+'</p>')
     })
     </script>
 </body>
--- chat/server_node/server.js
+++ chat/server_node/server.js
@@ -1,22 +1,9 @@
 var io = require('socket.io')(6001)
-console.log('connect to port 6001')
-io.on('error',function (socket){
-    console.log('error')
-})
-io.on('connection', function(socket){
-    console.log('Co nguoi vua ket noi ' + socket.id)
-})
 var Redis = require('ioredis')
 var redis = new Redis(6379)
-redis.psubscribe("*", function(error, count){
-
-}) 
-redis.on('pmessage', function(partner, channel, message){
-    console.log(channel)
-    console.log(message)
-    console.log(partner)
-
+redis.psubscribe("*", function (error, count) {
+})
+redis.on('pmessage', function (partner, channel, message) {
     message = JSON.parse(message)
     io.emit(channel, message.data.chat)
-    console.log('Sent')
 })
